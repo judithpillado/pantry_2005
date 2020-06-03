ed440771f71148af8b3587b977e414bb56796be1
Judith Pillado <judith.pillado@gmail.com>
2020-06-03 11:53:48 -0500

    Add ingredients test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -30,6 +30,13 @@ class RecipeTest < Minitest::Test
     @recipe1.add_ingredient(@ingredient1, 4)
     @recipe1.add_ingredient(@ingredient2, 8)
     assert_equal ({@ingredient1 => 6, @ingredient2 => 8}), @recipe1.ingredients_required
+  end
+
+  def test_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
   end 
 
 end
