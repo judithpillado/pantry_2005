fd029c812a7af321708c650df8d4d0ae6b015de9
Judith Pillado <judith.pillado@gmail.com>
2020-06-03 12:04:29 -0500

    Forgot to add class minitest but now add. Add recipe test

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -5,7 +5,8 @@ require './lib/pantry'
 require './lib/recipe'
 require './lib/cookbook'
 
-def setup
+class CookBookTest < Minitest::Test
+
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
@@ -17,7 +18,13 @@ def setup
   end
 
   def test_it_exists
-    assert_equal CookBook, @cookbook
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_add_recipe
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
 
 end
