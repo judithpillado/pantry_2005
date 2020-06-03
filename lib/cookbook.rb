212ee39cdeff53204f78ee35cd2965dbfde8d0d1
Judith Pillado <judith.pillado@gmail.com>
2020-06-03 12:41:19 -0500

    Add ingredients method

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -7,6 +7,15 @@ class CookBook
 
   def add_recipe(recipe)
     @recipes << recipe
-  end 
+  end
+
+  def ingredients
+    all_ingredients = @recipes.flat_map do |recipe|
+      recipe.ingredients
+    end
+    all_ingredients.map do |ingredient|
+      ingredient.name
+    end.uniq
+  end
 
 end
